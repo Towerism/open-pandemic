5b7085044100ff9180255f91d2e856b6b11e1d86
Martin Fracker <martin.frackerjr@gmail.com>
2015-07-07 02:41:21 -0500

    Append src to load path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,8 @@
 # The `.rspec` file also contains a few flags that are not defaults but that
 # users commonly want.
 #
+$LOAD_PATH << File.join('src')
+
 # Coverage configuration
 require 'simplecov'
 require 'coveralls'
