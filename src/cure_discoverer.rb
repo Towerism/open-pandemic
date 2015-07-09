210b8e28bbea03078df12cdf5494458c1e49c04b
Martin Fracker <martin.frackerjr@gmail.com>
2015-07-08 23:51:52 -0500

    Rearrange functions, reuse functions

--- src/cure_discoverer.rb
+++ src/cure_discoverer.rb
@@ -6,26 +6,26 @@ class Cure_discoverer
     @discovered = { :blue => false, :yellow => false, :red => false, :black => false }
   end
 
+  def discover_all
+    @discovered.each { |k, v| discover k }
+  end
+
   def discover disease
     check_disease_exists disease
     @discovered[disease] = true
   end
 
   def check_disease_exists disease
-    raise DiseaseError, 'No such disease' unless @discovered.has_key? disease
+    raise DiseaseError, "No such disease, '#{disease}'" unless @discovered.has_key? disease
   end
 
-  def discover_all
-    @discovered.each { |k, v| @discovered[k] = true }
+  def all_cures_discovered?
+    return @discovered.all? { |k, v| discovered? k }
   end
 
   def discovered? disease
     check_disease_exists disease
     @discovered[disease]
-  end
-
-  def all_cures_discovered?
-    return @discovered.all? { |k, v| @discovered[k] == true }
   end
 
   private :check_disease_exists
