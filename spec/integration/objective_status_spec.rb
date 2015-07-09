67a70d47eb2285699598e35df29933d75a286a1e
Martin Fracker <martin.frackerjr@gmail.com>
2015-07-09 13:41:58 -0500

    Add lose condition when player deck is empty

--- spec/integration/objective_status_spec.rb
+++ spec/integration/objective_status_spec.rb
@@ -1,9 +1,13 @@
 require 'objective_status'
 require 'cure_discoverer'
+require 'player_deck'
 
 RSpec.describe Objective_status, ".get" do
   let(:cure_discoverer) { Cure_discoverer.new }
-  subject { Objective_status.new cure_discoverer}
+  let(:player_deck) { Player_deck.new }
+  subject { Objective_status.new cure_discoverer, player_deck }
+
+  before(:each) { player_deck.add Player_card.new }
 
   context "When all cures have been discovered" do
     it "Returns win" do
@@ -11,7 +15,13 @@ RSpec.describe Objective_status, ".get" do
       expect(subject.get).to eq :win
     end
   end
-  context "When not all cures have been discovered" do
+  context "When the player deck is empty" do
+    it "Returns lose" do
+      player_deck.discard_all_cards
+      expect(subject.get).to eq :lose
+    end
+  end
+  context "When neither lost nor won" do
     it "Returns okay" do
       expect(subject.get).to eq :okay
     end
--- src/objective_status.rb
+++ src/objective_status.rb
@@ -1,6 +1,7 @@
 class Objective_status
-  def initialize cure_discoverer
+  def initialize cure_discoverer, player_deck
     @cure_discoverer = cure_discoverer
+    @player_deck = player_deck
   end
 
   def get
@@ -9,6 +10,7 @@ class Objective_status
 
   def check_win
     return :win if @cure_discoverer.all_cures_discovered?
+    return :lose if @player_deck.empty?
     :okay
   end
 end
