750cb1e826550bb1d772e5b5c1264b1d74d3e226
Martin Fracker <martin.frackerjr@gmail.com>
2015-07-09 00:48:16 -0500

    Refactor tests

--- spec/unit/cure_discoverer_spec.rb
+++ spec/unit/cure_discoverer_spec.rb
@@ -1,33 +1,29 @@
 require 'cure_discoverer'
 
-RSpec.describe Cure_discoverer, "#discover" do
-  context "When the specified disease has not yet been cured" do
-    it "Cures the specified disease" do
-      cure_discoverer = Cure_discoverer.new
-      cure_discoverer.discover :blue
-      expect(cure_discoverer.discovered? :blue).to eq true
+RSpec.describe Cure_discoverer, ".discover" do
+  context "When the disease has not yet been cured" do
+    it "Cures the disease" do
+      subject.discover :blue
+      expect(subject.discovered? :blue).to eq true
     end
   end
-  context "When the specified disease does not exist" do
-    it "Throws DiseaseError" do
-      cure_discoverer = Cure_discoverer.new
-      expect { cure_discoverer.discover :non_existant_disease }.to raise_error DiseaseError
+  context "When the disease does not exist" do
+    it "Raises DiseaseError" do
+      expect { subject.discover :non_existant_disease }.to raise_error DiseaseError
     end
   end
 end
 
-RSpec.describe Cure_discoverer, "#all_cures_discovered?" do
+RSpec.describe Cure_discoverer, ".all_cures_discovered?" do
   context "When all diseases have been discovered" do
     it "Returns true" do
-      cure_discoverer = Cure_discoverer.new
-      cure_discoverer.discover_all
-      expect(cure_discoverer.all_cures_discovered?).to eq true
+      subject.discover_all
+      expect(subject.all_cures_discovered?).to eq true
     end
   end
   context "When not all diseases have been discovered" do
     it "Returns false" do
-      cure_discoverer = Cure_discoverer.new
-      expect(cure_discoverer.all_cures_discovered?).to eq false
+      expect(subject.all_cures_discovered?).to eq false
     end
   end
 end
